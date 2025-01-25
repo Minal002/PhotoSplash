056be0d6d2a1c7c17c5a72450913dbe7cdd4773c
Minal002 <minal2576@gmail.com>
2025-01-25 21:04:05 +0900

    FEAT: 🎸 헤더 스타일 적용

--- react-album/src/components/common/header/CommonHeader.tsx
+++ react-album/src/components/common/header/CommonHeader.tsx
@@ -1,10 +1,10 @@
-import styles from './CommonHeader.module.css'
+import styles from './CommonHeader.module.scss'
 function CommonHeader() {
   return (
     <div className={styles.header}>
-        <div className={styles.header__longBox}>
-            <img src="" alt="" />
-            <span className={styles.header__longBox__title}>PhotoSplash</span>
+        <div className={styles.header__logoBox}>
+            <img src="src/assets/images/fish.png" alt="" className={styles.header__logoBox__logo}/>
+            <span className={styles.header__logoBox__title}>PhotoSplash</span>
         </div>
         <div className={styles.header__profileBox}>
             <button className={styles.header__profileBox__button}>사진제출</button>
